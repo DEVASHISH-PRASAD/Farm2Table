7963c1956f6874faf9f1084fb876efd4d19a41f0
Dev <128153292+DEVASHISH-PRASAD@users.noreply.github.com>
2025-01-18 22:46:21 +0530

    Added /public/_redirects for netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,  // Set the port to 4000 for local development
-  },
   build: {
     // Adjust the chunk size warning limit (default is 500 kB)
     chunkSizeWarningLimit: 1000, // Increases the limit to 1000 kB (1 MB)
     
     rollupOptions: {
       output: {
-        // Custom chunk splitting strategy
+        // Custom chunk splitting strateg
         manualChunks(id) {
           if (id.includes('node_modules/react')) {
             return 'react'; // Create a chunk for React
