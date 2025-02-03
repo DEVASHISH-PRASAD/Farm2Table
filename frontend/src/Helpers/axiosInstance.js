d7edb2be548548b3bb427d59e696a07f59016ad1
Dev <128153292+DEVASHISH-PRASAD@users.noreply.github.com>
2025-02-03 16:57:34 +0530

    Inventory management after product purchase

--- frontend/src/Helpers/axiosInstance.js
+++ frontend/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const BASE_URL="https://farm2table-2.onrender.com/api/v1"; 
-const BASE_URL="http://localhost:3001/api/v1";
+const BASE_URL="https://farm2table-2.onrender.com/api/v1"; 
+//const BASE_URL="http://localhost:3001/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL=BASE_URL;
